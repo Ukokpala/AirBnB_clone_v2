409a9ab695a0951f3137a252da56b61ada8b25db
ukokpala <okpalaukamaka480@gmail.com>
2023-05-20 22:32:55 +0000

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,14 @@
 start Flask application
 """
 
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
-def hello():
+def index():
     """returns Hello HBNB!"""
-    return render_template("10-hbnb_filters.html")
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=None)
+    app.run(host='0.0.0.0', port=5000)
